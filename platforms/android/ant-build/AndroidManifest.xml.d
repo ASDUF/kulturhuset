51bcf9cff54a4b1b81c7b7ea994fc7d1f2eeb5b3
Robert Gustafsson <zvaki97@hotmail.com>
2015-10-12 09:01:14 +0200

    vet ej

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/AndroidManifest.xml \
+/Users/Robbanz/documents/3an/Gymnasiearbete/appen/kulturhuset/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/Robbanz/documents/3an/Gymnasiearbete/appen/kulturhuset/platforms/android/AndroidManifest.xml \
