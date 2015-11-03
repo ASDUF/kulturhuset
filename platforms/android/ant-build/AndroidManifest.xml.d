06db739ee48ad6de36157ff19c8008b334382baa
Prohazz <ProHazz@Hanss-MacBook-Air.local>
2015-11-03 12:51:49 +0100

    huehue

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 /Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/ant-build/AndroidManifest.xml : \
 /Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/AndroidManifest.xml \
+=======
+/Users/ProHazz/desktop/Gymnasiearbete/appen/kulturhuset/kulturhuset/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/ProHazz/desktop/Gymnasiearbete/appen/kulturhuset/kulturhuset/platforms/android/AndroidManifest.xml \
+>>>>>>> Stashed changes
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 /Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
 /Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/ant-build/CordovaApp.ap_ \
 /Users/Robbanz/documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/ant-build/classes.dex \
 /Users/Robbanz/Documents/3an/Gymnasiearbete/Appen/kulturhuset/platforms/android/CordovaLib/ant-build/classes.jar \
+=======
+/Users/ProHazz/desktop/Gymnasiearbete/appen/kulturhuset/kulturhuset/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/ProHazz/desktop/Gymnasiearbete/appen/kulturhuset/kulturhuset/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/ProHazz/desktop/Gymnasiearbete/appen/kulturhuset/kulturhuset/platforms/android/ant-build/classes.dex \
+/Users/ProHazz/Desktop/Gymnasiearbete/Appen/kulturhuset/kulturhuset/platforms/android/CordovaLib/ant-build/classes.jar \
+>>>>>>> Stashed changes
